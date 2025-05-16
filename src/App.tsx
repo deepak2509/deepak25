c8addcf3c0a19edede8a49c11e7134cda9711b08
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2025-05-16 10:02:34 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import ScrollToHash from "./components/ScrolltoHash";
+import ScrolltoHash from "./components/ScrolltoHash";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -14,6 +14,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <HashRouter>
+        <ScrolltoHash />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
