9fc3e71012b16b3128b4508107db76e7f4735118
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2025-05-16 09:24:01 -0500

    Update ScrolltoHash.tsx

--- src/ScrolltoHash.tsx
+++ src/ScrolltoHash.tsx
@@ -1,1 +1,23 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToHash = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const el = document.querySelector(location.hash);
+      if (el) {
+        // Delay ensures it scrolls after content renders
+        setTimeout(() => {
+          el.scrollIntoView({ behavior: "smooth" });
+        }, 300);
+      }
+    }
+  }, [location]);
+
+  return null;
+};
+
+export default ScrollToHash;
 
